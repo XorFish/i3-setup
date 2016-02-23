f9c659c337b94a32adf46fba6456ddeeb4764bea
t-moe <timolang@gmail.com>
2016-02-23 17:34:34 +0100

    Fixed some small bugs in setup and wifi script.

--- setup_i3.sh
+++ setup_i3.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/bash
 
+set -e #abort on error
 
-sudo pacman -S --noconfirm i3 dash feh i3lock rofi compton imagemagick scrot
-ruby -e "$(curl -fsSL https://raw.github.com/supermarin/YosemiteSanFranciscoFont/master/install)"
+sudo pacman -Sy --noconfirm i3 dash feh i3lock rofi compton imagemagick scrot
 yaourt -S --noconfirm ttf-font-awesome i3blocks-git
+ruby -e "$(curl -fsSL https://raw.github.com/supermarin/YosemiteSanFranciscoFont/master/install)"
 
-sudo mkdir /usr/lib/i3blocks
-sudo cp ./scripts/* /usr/lib/i3blocks/
+sudo mkdir -p  /usr/lib/i3blocks
+sudo cp -r ./scripts/* /usr/lib/i3blocks/
 
-mkdir $HOME/.config/i3blocks
-mkdir $HOME/.config/i3
-mkdir $HOME/.wallpapers
-mkdir $HOME/screenshots
+mkdir -p $HOME/.config/i3blocks
+mkdir -p $HOME/.config/i3
+mkdir -p $HOME/.wallpaper
+mkdir -p $HOME/screenshots
 
-cp ./i3blocks.conf $HOME/.config/i3blocks/conf
+cp ./i3blocks.conf $HOME/.config/i3blocks/config
 cp ./config $HOME/.config/i3/config
 cp ./i3lock-wrapper $HOME/.config/i3/i3lock-wrapper
-cp ./wallpapers/ $HOME/.wallpaper/*
+cp ./wallpaper/ $HOME/.wallpaper/*
